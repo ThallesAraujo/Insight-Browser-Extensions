f65e9aed40db3c7cf7bbdb4565abbaf82a0aee7d
Thalles Araújo <thalleshenrique.na@gmail.com>
2021-04-24 13:55:57 -0300

    teste

--- thalles-dark-mode.js
+++ thalles-dark-mode.js
@@ -24,8 +24,8 @@
     const applyDark = () => {
       DarkReader.setFetchMethod(window.fetch)
       DarkReader.auto({
-        brightness: 0,
-        contrast: 100,
+        brightness: 100,
+        contrast: 90,
         sepia: 0
       });
     };
