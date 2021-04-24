06ea1208fd096849b33c70756116b2f5aa62bd85
Thalles Araújo <thalleshenrique.na@gmail.com>
2021-04-24 14:10:31 -0300

    Inverter sem imagens

--- thalles-dark-mode-2.js
+++ thalles-dark-mode-2.js
@@ -1,26 +1,24 @@
-javascript: (
-  function () { 
-  // the css we are going to inject
-  var css = 'html {-webkit-filter: invert(100%);' +
-      '-moz-filter: invert(100%);' + 
-      '-o-filter: invert(100%);' + 
-      '-ms-filter: invert(100%); }',
-  
-  head = document.getElementsByTagName('head')[0],
-  style = document.createElement('style');
-  
-  // a hack, so you can "invert back" clicking the bookmarklet again
-  if (!window.counter) { window.counter = 1;} else  { window.counter ++;
-  if (window.counter % 2 == 0) { var css ='html {-webkit-filter: invert(0%); -moz-filter:    invert(0%); -o-filter: invert(0%); -ms-filter: invert(0%); }'}
-   };
-  
-  style.type = 'text/css';
-  if (style.styleSheet){
-  style.styleSheet.cssText = css;
-  } else {
-  style.appendChild(document.createTextNode(css));
-  }
-  
-  //injecting the css to the head
-  head.appendChild(style);
-  }());
+//set up color properties to iterate through
+var colorProperties = ['color', 'background-color'];
+
+//iterate through every element in reverse order...
+$("*").get().reverse().each(function() {
+    var color = null;
+
+    for (var prop in colorProperties) {
+        prop = colorProperties[prop];
+
+        //if we can't find this property or it's null, continue
+        if (!$(this).css(prop)) continue; 
+
+        //create RGBColor object
+        color = new RGBColor($(this).css(prop));
+
+        if (color.ok) { 
+            //good to go, let's build up this RGB baby!
+            //subtract each color component from 255
+            $(this).css(prop, 'rgb(' + (255 - color.r) + ', ' + (255 - color.g) + ', ' + (255 - color.b) + ')');
+        }
+        color = null; //some cleanup
+    }
+});
